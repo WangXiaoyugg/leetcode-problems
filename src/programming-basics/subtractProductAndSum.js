31ec19152b514ea5c6aba8322cad9cf2c6a943d9
xywang71 <xywang71@iflytek.com>
2025-02-18 19:17:29 +0800

    feat: 【1281】整数的各位积和之差

---
+++ src/programming-basics/subtractProductAndSum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var subtractProductAndSum = function(n) {
+    let sum = 0;
+    let product = 1;
+    while(n > 0) {
+        let mod = (n % 10);
+        sum += mod;
+        product *= mod;
+        n = Math.floor(n / 10);
+    }
+    return product - sum;
+};
